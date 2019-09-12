f76875bc46e9fca6a272533c2efb40c47415d3ee
Maksimka <52958787+andreevmaxi@users.noreply.github.com>
2019-09-12 12:07:17 +0300

    2.0

--- fuzzy_logic/main.cpp
+++ fuzzy_logic/main.cpp
@@ -4,48 +4,55 @@
 
 
 
-bool UserInputFunc(const int MaxInput, float* x1, float* x2, char* operation);
+bool UserInputFunc(float* x1, float* x2, char* operation);
 
 bool Operation(float* x1, float* x2, char* oper);
 
-bool Not(float* a);
+float Not(float* a);
 
-bool Or(float* a, float* b);
+float Or(float* a, float* b);
 
-bool And(float* a, float* b);
+float And(float* a, float* b);
 
 int main()
 {
-    const int MaxInput = 128;
 
     float x1 = 0;
     float x2 = 0;
 
     char operation = '\0';
 
-    UserInputFunc(MaxInput, &x1, &x2, &operation);
+    UserInputFunc(&x1, &x2, &operation);
 
     Operation(&x1, &x2, &operation);
 
     return 0;
 }
 
-bool UserInputFunc(const int MaxInput, float* x1, float* x2, char* operation)
+bool UserInputFunc(float* x1, float* x2, char* operation)
 {
     assert(x1 != NULL);
     assert(x2 != NULL);
     assert(operation != NULL);
 
-    char* UserInput = (char*)calloc ( (MaxInput), sizeof(char));
+    int CharSize = 0;
+
+    FILE* f = fopen("input.txt", "r");
+    fseek(f, 0L, SEEK_END);
+    CharSize = ftell(f);
+    rewind(f);
+
+    char* UserInput = (char*)calloc ( (CharSize), sizeof(char));
     assert (UserInput != NULL);
 
-    scanf("%s", UserInput);
+    fgets(UserInput, CharSize+1 , f);
+    fclose(f);
 
     bool NowInputVariable = 1;
     int NowTen = 1;
     int NowElement = 0;
 
-    while ( (NowElement < MaxInput) && (*(UserInput + NowElement) != '\0' && *(UserInput + NowElement) != '\n' && *(UserInput + NowElement) != 'EOF' ) )
+    while ( (NowElement < CharSize) && (*(UserInput + NowElement) != '\0' && *(UserInput + NowElement) != '\n' && *(UserInput + NowElement) != 'EOF' ) )
     {
         if (*(UserInput + NowElement) >= '0' && *(UserInput + NowElement) <= '9')
         {
@@ -90,52 +97,53 @@ bool Operation(float* x1, float* x2, char* oper)
     assert(x2 != NULL);
     assert(oper != NULL);
 
+    float tmp = 0;
     switch(*oper)
     {
         case '\0':
         return 0;
 
         case '~':
-        Not(x1);
+        tmp = Not(x1);
         break;
 
         case '+':
-        Or(x1, x2);
+        tmp = Or(x1, x2);
         break;
 
         case '*':
-        And(x1, x2);
+        tmp = And(x1, x2);
         break;
     }
 
+    FILE* f = fopen("output.txt", "w");
+    fprintf(f, "%.2f", tmp);
+    fclose(f);
     return 1;
 }
 
-bool Not(float* a)
+float Not(float* a)
 {
     float tmp = 0;
 
     tmp = 1 - *a;
-    printf("%.2f", tmp);
-    return 1;
+    return tmp;
 }
 
-bool Or(float* a, float* b)
+float Or(float* a, float* b)
 {
     float tmp = 0;
 
     tmp = (*a) + (*b) - (*a)*(*b);
-    printf("%.2f", tmp);
-    return 1;
+    return tmp;
 }
 
-bool And(float* a, float* b)
+float And(float* a, float* b)
 {
     float tmp = 0;
 
     tmp = (*a)*(*b);
-    printf("%.2f", tmp);
-    return 1;
+    return tmp;
 }
 
 
